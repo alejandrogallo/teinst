0050d8ae7993e1dfb7b1c5d6190a5f97af2840bb
Alejandro Gallo <aamsgallo@gmail.com>
2023-05-24 01:18:21 +0200

    Update test.lisp

--- test.lisp
+++ test.lisp
@@ -3,25 +3,8 @@
 (defvar *tsr* nil)
 
 (setq *tsr* (make-instance 'tensor
-                           :lengths '(50 60 70 800)
+                           :lengths '(5 6 7 8)
                            :type :double-float))
-
-(with-slots (handle) *tsr*
-  (mem-ref (%tensor-lengths-r-s (mem-ref handle :pointer 0) 4)
-           :size 0))
-
-(with-slots (handle) *tsr*
-  (mem-ref (%tensor-lengths-r-s handle 4)
-           :size 0))
-
-(with-slots (handle) *tsr*
-  (mem-aref (%tensor-lengths-r-s handle 4)
-           :size 3))
-
-(with-slots (handle) *tsr*
-  (convert-from-foreign (%tensor-lengths-r-s handle 4)
-                        '(:array :size 4)))
-
 
 (with-slots (handle lengths) *tsr*
   (assert handle)
@@ -33,19 +16,7 @@
                        lens)
     (loop for i below 4
           do (assert (= (mem-aref lens :size i)
-                        (nth i lengths))))))
-
-
-(with-slots (handle lengths) *tsr*
-  (assert handle)
-  (assert lengths)
-  (assert (equal lengths '(5 6 7 8)))
-  (with-foreign-pointer (lens (* 8 (length lengths)))
-    (%tensor-lengths-s handle
-                       (length lengths)
-                       lens)
-    (loop for i below 4
-          collect (mem-aref lens :size i)))
+                        (nth i lengths)))))
   (with-foreign-pointer (name 8)
     (format t "previous memory ~a ~a~%" name (mem-aref name :pointer 0))
     (%tensor-name handle name)
